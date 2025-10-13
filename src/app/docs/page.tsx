bfbcc6f8fd06012dd83c388b43232a132656fcef
Anthony Cueva <hi@cueva.io>
2025-10-13 18:10:40 -0500

    perf: ssr api docs. move OpenAPI docs to src/app/docs and update page to use local spec file

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,17 +1,20 @@
-"use client";
-
 import { ApiReferenceReact } from "@scalar/api-reference-react";
 
 import "@scalar/api-reference-react/style.css";
 
-export default function ApiDocsPage() {
+import content from "./openapi.json";
+
+export const revalidate = false;
+export const dynamic = "force-static";
+
+export default async function ApiDocsPage() {
   return (
     <ApiReferenceReact
       configuration={{
         _integration: "nextjs",
-        url: `${process.env.NEXT_PUBLIC_URL}/openapi.json`,
         hideModels: true,
         hideClientButton: true,
+        content,
       }}
     />
   );
