8d6b4114c4e63ccedc019749f3cb2485f5c81857
Anthony Cueva <hi@cueva.io>
2025-10-17 14:27:43 -0500

    style: increase LupaFullIcon size on homepage header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -77,7 +77,7 @@ export default async function Home() {
       <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
         <div className="container mx-auto flex h-14 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
           <Link href="/" className="px-2 py-0 items-center">
-            <LupaFullIcon className="size-12  md:size-16" />
+            <LupaFullIcon className="size-14  md:size-16" />
           </Link>
           <nav className="flex items-center gap-3 sm:gap-6">
             <a
